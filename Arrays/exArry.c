1059e456a08f0a88def5c9e46084b7f769330853
Mustapha Abourar <elmustapha.abourar@outlook.com>
2023-04-03 15:27:02 +0200

    Function Get Set and Sum ADT ARRAYS

--- Arrays/exArry.c
+++ Arrays/exArry.c
@@ -76,10 +76,26 @@ int Linear_Search(struct Array *m, int key)
     printf("Element not found \n");
     return -1;
 }
+ int Binary_Search(struct Array *m, int key)
+ {
+    int low = 0, height = m->length;
+    while(key <= height && key >= low)
+    {
+        int mid = (low + height) / 2;
+        if (key == m->A[mid])
+            return mid;
+        else if (key < m->A[mid])
+            height = mid - 1;
+        else
+            low = mid + 1;
+    }
+     return -1;
+ }
 
 int main()
 {
-    struct Array arr = {{2,3,5,4,8,6,9,8,1,3,10}, 11, 20};
+    struct Array arr = {{2,3,5,4,8,6,9,18,1,3,10}, 11, 20};
     Display(arr);
     Linear_Search(&arr, 6);
+    printf("The element %d",Binary_Search(&arr, 18));
 }
